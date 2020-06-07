d8b3218eee8f891612bcfd5b866a9508fc0205dc
cf2013 <carlosfrancisco.rg@gmail.com>
2020-06-06 19:22:09 -0500

    email try

--- web/index.php
+++ web/index.php
@@ -22,18 +22,4 @@ $app->get('/', function() use($app) {
   return $app['twig']->render('index.twig');
 });
 
-$from = new SendGrid\Email(null, "dream.english.contact@gmail.com");
-$subject = "Hello World from the SendGrid PHP Library!";
-$to = new SendGrid\Email(null, "carlosfrancisco.rg@gmail.com");
-$content = new SendGrid\Content("text/plain", "Hello, Email!");
-$mail = new SendGrid\Mail($from, $subject, $to, $content);
-
-$apiKey = getenv('SENDGRID_API_KEY');
-$sg = new \SendGrid($apiKey);
-
-$response = $sg->client->mail()->send()->post($mail);
-echo $response->statusCode();
-echo $response->headers();
-echo $response->body();
-
 $app->run();
