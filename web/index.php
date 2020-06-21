59bc6b8173d94b3a7db3200ab4c9b9e1805f1649
cf2013 <carlosfrancisco.rg@gmail.com>
2020-06-21 09:56:58 -0500

    test

--- web/index.php
+++ web/index.php
@@ -23,3 +23,16 @@ $app->get('/', function() use($app) {
 });
 
 $app->run();
+$from = new SendGrid\Email(null, "dream.english.contact@gmail.com");
+$subject = "Hello World from the SendGrid PHP Library!";
+$to = new SendGrid\Email(null, "carlosfrancisco.rg@gmail.com");
+$content = new SendGrid\Content("text/plain", "Hello, Email!");
+$mail = new SendGrid\Mail($from, $subject, $to, $content);
+
+$apiKey = getenv('SENDGRID_API_KEY');
+$sg = new \SendGrid($apiKey);
+
+$response = $sg->client->mail()->send()->post($mail);
+echo $response->statusCode();
+echo $response->headers();
+echo $response->body();
